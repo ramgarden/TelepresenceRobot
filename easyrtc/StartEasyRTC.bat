4e2c57c7f85dc162cb46e6b4e539b4a4a2b94b08
Josh P <ramgarden@gmail.com>
2015-07-15 20:00:33 -0500

    added StartEasyRTC.bat script
    used to start the server and the robot driver

---
+++ easyrtc/StartEasyRTC.bat
@@ -0,0 +1,9 @@
+start C:\Users\Josh\Documents\GitHub\TelepresenceRobot\src\iRobotCreateControl\bin\Debug\iRobotCreateControl.exe
+
+start node server.js
+
+TIMEOUT /T 10 /NOBREAK
+
+C:\Users\Josh\Desktop\TelemakerKiosk.lnk
+
+
