8388dae6c238e72bd5a1541d95ea7bac3cd12dd7
Josh P <ramgarden@gmail.com>
2016-10-10 21:00:39 -0400

    added create-oi package
    instead of trying to do the serial myself, just use other people's
    irobot create packages.

--- easyrtc/serverSerial.js
+++ easyrtc/serverSerial.js
@@ -5,6 +5,42 @@ var io      = require("socket.io");         // web socket external module
 var easyrtc = require("easyrtc");           // EasyRTC external module
 var bodyParser  = require('body-parser');
 var SerialPort = require("serialport");     // SerialPort external module
+var robot = require("create-oi");           // iRobot Create Open Interface
+ 
+robot.init({ serialport: "COM4" });
+
+//iRobot Create OpCodes
+var SoftReset = 7,
+var Start = 128,
+var Baud = 129,
+var Control = 130,
+var Safe = 131,
+var Full = 132,
+var Spot = 134,
+var Cover = 135,
+var Demo = 136,
+var Drive = 137,
+var LowSideDrivers = 138,
+var LEDs = 139,
+var Song = 140,
+var Play = 141,
+var Sensors = 142,
+var CoverAndDock = 143,
+var PWMLowSideDrivers = 144,
+var DriveDirect = 145,
+var DigitalOutputs = 147,
+var Stream = 148,
+var QueryList = 149,
+var PauseResumeStream = 150,
+var SendIR = 151,
+var Script = 152,
+var PlayScript = 153,
+var ShowScript = 154,
+var WaitTime = 155,
+var WaitDistance = 156,
+var WaitAngle = 157,
+var WaitEvent = 158,
+
 
 // Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
 var httpApp = express();
@@ -24,17 +60,23 @@ var socketServer = io.listen(webServer, {"log level":1});
 var rtc = easyrtc.listen(httpApp, socketServer);
 
 // Start up the Serial Port for talking directly to the iRobot Create (TM)
-var port = new SerialPort('COM4', function (err) {
+var port = new SerialPort('COM4', {baudRate: 57600}, function (err) {
   if (err) {
     return console.log('Error: ', err.message);
   }
-  port.write('main screen turn on', function(err) {
-    if (err) {
-      return console.log('Error on write: ', err.message);
-    }
-    console.log('message written');
-  });
 });
+
+writeSerial(Start);
+
+function writeSerial(message) {
+	port.write(message, function(err) {
+	if (err) {
+	  return console.log('Error on write: ', err.message);
+	}
+	console.log('message written:\n' + message);
+	});
+}
+
 
 httpApp.post('/login', function(req, res) {
   console.log("Got password: " + req.body.password);
@@ -47,6 +89,9 @@ httpApp.post('/forward', function(req, res) {
     console.log('Forward button pressed!');
 	//125 -25  W
     SendUDP("125 -25");
+	robot.on('ready', function() {
+		this.drive(100,32767);
+	});
 });
 
 httpApp.post('/left', function(req, res) {
